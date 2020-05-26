0efe1693c26c7c3a7c133fb57b42040a24c5af62
nooshines <nooshin_es26413@yahoo.com>
2020-05-26 21:06:06 +1000

    getting data from api

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-//on ready
-$(() => {});
-
-//search cocktail
-function searchCocktail(e) {
-  e.preventDefault();
-  const searchTerm = $("#search").val();
-  console.log(searchTerm);
+async function getCocktails(searchTerm) {
+  const Cocktails = await $.ajax(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+  );
+  console.log(Cocktails);
 }
 
-//event listeners
-$("#submit").on("submit", searchCocktail);
+//on ready
+$(() => {
+  $("#submit").on("submit", (e) => {
+    e.preventDefault();
+    $("#search").empty();
+    console.log("search works");
+    const searchTerm = $("#search").val();
+    getCocktails(searchTerm);
+  });
+});
