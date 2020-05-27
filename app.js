cbbbda3081ed7c55cc0fa600ccc666d0f955c1b2
nooshines <nooshin_es26413@yahoo.com>
2020-05-27 10:13:55 +1000

    worked on empty and invalid input messages

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 async function getCocktails(searchTerm) {
-  const Cocktails = await $.ajax(
+  const response = await $.ajax(
     `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
-  );
-  console.log(Cocktails);
+  ).catch((e) => {
+    console.log(e);
+  });
+  console.log(response);
+
+  //updating UI
+  showCocktails(response, searchTerm);
+}
+
+function showCocktails(data, title) {
+  //if type something that is not valid or is not in the api I want to give me feedback
+  if (data.drinks === null) {
+    $("#result-heading").append(
+      `<p class="text-white m-5">There are no search result for '${title}' ,try again!</p>`
+    );
+  } else {
+    $("#result-heading").empty();
+    $("#result-heading").append(
+      `<h4 class="text-white m-5">Search Result for ${title}:</h4>`
+    );
+  }
 }
 
 //on ready
 $(() => {
   $("#submit").on("submit", (e) => {
     e.preventDefault();
+    $("#alert").empty();
     $("#search").empty();
-    console.log("search works");
     const searchTerm = $("#search").val();
-    getCocktails(searchTerm);
+    //give alert if we dont put anything and press enter
+    if (searchTerm.trim()) {
+      getCocktails(searchTerm);
+    } else {
+      $("#alert").append(
+        `<p class="text-danger m-3">please enter a search term .</p>`
+      );
+    }
   });
 });
